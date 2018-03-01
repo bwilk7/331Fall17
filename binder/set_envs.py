52152c9a056e8059c730b8f587bef6daed552689
Bryan Wilkinson <bwilk7@gmail.com>
2018-03-01 09:44:20 -0500

    Adding LuaRocks to Binder

---
+++ binder/set_envs.py
@@ -0,0 +1,18 @@
+#!/bin/python
+import sys
+import json
+
+kernel_path = sys.argv[1]
+variables_path = sys.argv[2]
+
+kernel = json.load(open(kernel_path))
+env = {}
+
+with open(variables_path) as f:
+	for line in f:
+		parts = line.strip().split(":")
+		if parts:
+			env[parts[0]] = parts[1]
+
+kernel['env'] = env
+json.dump(kernel,open(kernel_path,'w'))
